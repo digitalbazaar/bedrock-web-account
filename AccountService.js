9ccf5f597270f1b60289187236ae4d45767ffe1a
Dave Longley <dlongley@digitalbazaar.com>
2018-05-14 16:29:05 -0400

    Add core files.

---
+++ AccountService.js
@@ -0,0 +1,39 @@
+/*!
+ * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import axios from 'axios';
+
+export class AccountService {
+  constructor({
+    urls = {
+      base: '/accounts'
+    }
+  }) {
+    this.config = {urls};
+  }
+
+  async exists({url = this.config.urls.base, email}) {
+    try {
+      const response = await axios.get(url, {
+        params: {
+          exists: true,
+          email
+        }
+      });
+      return true;
+    } catch(e) {}
+    return false;
+  }
+
+  async create({url = this.config.urls.base, email}) {
+    const response = await axios.post(url, {email});
+    return response.data;
+  }
+
+  async get({url = this.config.urls.base, id}) {
+    const response = await axios.get(url + '/' + id);
+    return response.data;
+  }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# bedrock-web-account ChangeLog
+
+## 0.0.0 - 2018-05-14
+
+### Added
+- Add core files.
+
+- See git history for changes previous to this release.
